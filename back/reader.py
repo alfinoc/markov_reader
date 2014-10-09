4bb7caafa9f25c466e61e4c3f2af32663b45e486
alfinoc <chrisalfino@gmail.com>
2014-10-09 01:28:08 -0700

    added Redis index serialization scheme and first implementation

--- back/reader.py
+++ back/reader.py
@@ -1,6 +1,8 @@
 from bisect import bisect_left
 from random import uniform
 from collections import OrderedDict
+from copy import copy
+import redis
 
 """
 Locate the leftmost value in 'a' exactly equal to 'x'
@@ -15,6 +17,10 @@ def index(a, x):
 TERMINATOR = list('.?,!:;')
 QUOTES = list('"')
 
+
+"""
+A scanner for reading from a source text Index.
+"""
 class Reader:
    """
    builds a reader around the provided index, 
@@ -23,7 +29,6 @@ class Reader:
       self.index = index
       # TODO: lol this won't work in general
       self.last = self.index.last
-      print self.last, self.index.getTerm(self.last)
 
    """
    let p be the last phrase returned. a subsequent call to "next()" returns a successor s
@@ -73,3 +78,122 @@ class Reader:
             return probabilityThresholds[prob]
 
       raise ValueError
+
+"""
+Locate the leftmost value in 'a' exactly equal to 'x'
+"""
+def index(a, x):
+   i = bisect_left(a, x)
+   if i != len(a) and a[i] == x:
+      return i
+   raise ValueError
+
+#DEBUG = False  # makes token indexing more transparent
+TERMINATOR = list('.?,!:;')
+QUOTES = list('"')
+
+"""
+A pre-parsed version of the source text contained in 'filename'. Each whitespace separated
+unique word in the source is given an ID, and for any given word-id w, a collection of
+words that follow w can be retrived in the form of a frequency distribution.
+"""
+class Index:
+   def parseFile(self, filename):
+      tokens = self.__getTokens(open(filename))
+      self.filename = filename
+      self.dictionary = list(set(tokens))
+      #TODO: necessary? not if the index is just an ID
+      self.dictionary.sort()
+      self.successors = self.__getFrequencyMap(tokens)
+      #TODO: self.last should go in reader entirely
+      self.last = index(self.dictionary, tokens[0]) if len(tokens) != 0 else None
+
+   """
+   returns a map from id to count, where each id is a successor (appears immediately after
+   'termId' in the source text) and the count is the number of times that the successor
+   follows the argument term id.
+   """
+   def getSuccessors(self, termId):
+      return self.successors[termId]
+
+   """
+   returns the string term with the given ID
+   """
+   def getTerm(self, termId):
+      if 0 <= termId and termId < len(self.dictionary):
+         return self.dictionary[termId]
+      else:
+         return None
+
+   """
+   writes the index to the provided Redis store with the following <k,v> format:
+      filename:id -> term_string
+      filename:id:succ -> HASH<id, count>
+      filename:term -> id
+   with id being the term id for every term stored in the index.
+   """
+   def serialize(self, store):
+      #TODO: instead of using the filename as a namespace, instead use some ID
+      #      that way we can use different files with the same name.
+      for i in range(0, len(self.dictionary)):
+         store.set(self.filename + ':' + str(i), self.dictionary[i])
+      for keyId in self.successors:
+         copy = dict()
+         for succ in self.successors[keyId]:
+            copy[str(succ)] = str(self.successors[keyId][succ])
+         store.hmset(self.filename + ':' + str(keyId) + ':succ', copy)
+
+   """
+   returns the whitespace tokens from the file, making each terminator punctuator its own
+   token and removing all quotes (double and single)
+   """
+   def __getTokens(self, file):
+      tokens = []
+      for line in file:
+         # handle terminators and quotes specially
+         line = list(line)
+         line = filter(lambda char : not char in QUOTES, line)
+         for i in range(len(line)):
+            if line[i] in TERMINATOR:
+               line.insert(i, ' ')
+               i += 1
+         line = ''.join(line)
+
+         tokens += line.split()
+
+      # TODO: correct capitalization on non-names
+
+      return tokens
+
+   """
+   returns a map of token to successor map, where each successor map maps a successor
+   to its number of occurrances in tokens. in other words, for every successive pair
+   of tokens (a, b) and returned map m, m[a][b] is the number of times "a b" appears
+   in immediate sequence in tokens
+
+   the returned map uses integer indices to identify tokens. these indices correspond to
+   the values stored in self.dictionary
+   """
+   def __getFrequencyMap(self, tokens):
+      result = {}
+      for i in range(len(tokens) - 1):
+         first  = index(self.dictionary, tokens[i])      # (tokens[i], i)
+         second = index(self.dictionary, tokens[i + 1])  # (tokens[i + 1], i)
+         if not first in result:
+            result[first] = {}
+         prevSuccessors = result[first]
+         if not second in prevSuccessors:
+            prevSuccessors[second] = 1
+         else:
+            prevSuccessors[second] += 1
+      return result
+
+"""
+   notes:
+   you're going to want to incorporate a dec english dictionary here. for capitalization,
+   only store the lowercase version for dictionary words (exclude names).
+
+   going to need to handle terminator punctuation in a special way
+   fuck am i going to do with --? ...?
+
+"""
