5e564ff2c85bc311a0bebabd83a9ff1d66916e84
alfinoc <chrisalfino@gmail.com>
2014-12-03 00:47:25 -0800

    actually looking kinda dope

--- front/externs.js
+++ front/externs.js
@@ -1,5 +1,5 @@
-window.COLORS = ['#218693', '#C75646'];
-
+window.COLORS = ['#1565C0', '#00695C',  '#4527A0']; // '#218693' '#C75646'
+// 1:blue 2:green
 window.getNextColor = function() {
    var next = window.COLORS.pop();
    window.COLORS.unshift(next);
