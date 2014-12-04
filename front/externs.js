bc7f59dee505e69def480e831cef9bdfbd963c03
alfinoc <chrisalfino@gmail.com>
2014-12-03 16:32:04 -0800

    added source radio menu

--- front/externs.js
+++ front/externs.js
@@ -1,4 +1,4 @@
-window.COLORS = ['#1565C0', '#00695C',  '#4527A0']; // '#218693' '#C75646'
+window.COLORS = ['#C75646', '#218693', '#AA82B3']; // '#1565C0' '#00695C',  purp'#4527A0'
 // 1:blue 2:green
 window.getNextColor = function() {
    var next = window.COLORS.pop();
