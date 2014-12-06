b8a3927258ee432152e893d69b043305f7c2e994
alfinoc <chrisalfino@gmail.com>
2014-12-05 18:18:17 -0800

    progress with gradient borders and a new coloring scheme. more ugly css but better than externs, i think

--- front/externs.js
+++ front/externs.js
@@ -1,7 +1,18 @@
 window.COLORS = ['#C75646', '#218693', '#AA82B3']; // '#1565C0' '#00695C',  purp'#4527A0'
-// 1:blue 2:green
+
 window.getNextColor = function() {
    var next = window.COLORS.pop();
    window.COLORS.unshift(next);
    return next;
+}
+
+window.TERMINATORS = '.?!'
+window.BRIDGE = ',:;'
+
+window.isTerminator = function(str) {
+   return window.TERMINATORS.indexOf(str) != -1;
+}
+
+window.isBridge = function(str) {
+   return window.BRIDGE.indexOf(str) != -1;
 }
